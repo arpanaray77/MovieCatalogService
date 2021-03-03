5000ff97522e5c38bf7b1a171c8399bf5bb624ae
arpanaray77 <arpanaray75897@gmail.com>
2021-03-03 08:18:48 +0530

    Communicating between microservices

--- Rating-Movie-service/src/main/java/com/learnmicroservices/RatingMovieservice/controller/RatingmovieController.java
+++ Rating-Movie-service/src/main/java/com/learnmicroservices/RatingMovieservice/controller/RatingmovieController.java
@@ -1,10 +1,14 @@
 package com.learnmicroservices.RatingMovieservice.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.learnmicroservices.RatingMovieservice.model.RatingmovieItem;
+import com.learnmicroservices.RatingMovieservice.model.UserRating;
 
 @RestController
 @RequestMapping("/rating")
@@ -15,5 +19,20 @@ public class RatingmovieController {
 	{
 		return new  RatingmovieItem(movieid,4);
 		
+	}
+	
+	//to return username along with rating list
+	@RequestMapping("users/{userid}")
+	public UserRating getUserRating(@PathVariable("userid") String userid)
+	
+	{
+	List<RatingmovieItem> ratings = Arrays.asList(
+			new RatingmovieItem("1234",4),
+			new RatingmovieItem("5678",5)
+		);
+		
+	UserRating userRating=new UserRating();
+	userRating.setUserRating(ratings);
+	return userRating;
 	}
 }
