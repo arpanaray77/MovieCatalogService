e82d8defc265b617830759c3bf3800358eef849c
arpanaray77 <arpanaray75897@gmail.com>
2021-03-07 21:01:28 +0530

    Registering Services on Eureka server

--- Rating-Movie-service/src/main/java/com/learnmicroservices/RatingMovieservice/controller/RatingmovieController.java
+++ Rating-Movie-service/src/main/java/com/learnmicroservices/RatingMovieservice/controller/RatingmovieController.java
@@ -3,6 +3,7 @@ package com.learnmicroservices.RatingMovieservice.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.learnmicroservices.RatingMovieservice.model.UserRating;
 
 @RestController
 @RequestMapping("/rating")
+@EnableEurekaClient
 public class RatingmovieController {
 
 	@RequestMapping("/{movieid}")
