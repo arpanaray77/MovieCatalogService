0d2d09ab996789ac58cef6970b12c21e56becabb
arpanaray77 <arpanaray75897@gmail.com>
2021-03-07 21:00:51 +0530

    Registering Services on Eureka server

--- Movie-info-service/src/main/java/com/learnmicroservices/Movieinfoservice/controller/MovieinfoController.java
+++ Movie-info-service/src/main/java/com/learnmicroservices/Movieinfoservice/controller/MovieinfoController.java
@@ -1,5 +1,6 @@
 package com.learnmicroservices.Movieinfoservice.controller;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import com.learnmicroservices.Movieinfoservice.model.MovieinfoItem;
 
 @RestController
 @RequestMapping("/movie")
+@EnableEurekaClient
 public class MovieinfoController {
 
 	@RequestMapping("/{movieid}")
