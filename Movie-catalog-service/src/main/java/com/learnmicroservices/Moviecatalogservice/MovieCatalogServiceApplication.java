d2068af7c38eb065b8ff3d568f7ed770d5afded0
arpanaray77 <arpanaray75897@gmail.com>
2021-04-03 18:24:45 +0530

    Adding Cloud Config server

--- Movie-catalog-service/src/main/java/com/learnmicroservices/Moviecatalogservice/MovieCatalogServiceApplication.java
+++ Movie-catalog-service/src/main/java/com/learnmicroservices/Moviecatalogservice/MovieCatalogServiceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +11,6 @@ import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
-@EnableConfigServer
 @EnableEurekaClient
 @EnableCircuitBreaker
 @EnableHystrixDashboard
