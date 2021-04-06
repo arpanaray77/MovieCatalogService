fefcbc468b7e89a98c256c35423fd4e0697a471d
arpanaray77 <arpanaray75897@gmail.com>
2021-04-06 22:04:56 +0530

    Adding Spring Security Feature

--- Movie-catalog-service/src/main/java/com/learnmicroservices/Moviecatalogservice/SecurityConfig.java
+++ Movie-catalog-service/src/main/java/com/learnmicroservices/Moviecatalogservice/SecurityConfig.java
@@ -1,9 +1,15 @@
 package com.learnmicroservices.Moviecatalogservice;
 
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -20,6 +26,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			     	 .loginPage("/login")
 		             .permitAll();
         }
+        
+        @Bean
+    	@Override
+    	public UserDetailsService userDetailsService() {
+    		UserDetails user =
+    			 User.withDefaultPasswordEncoder()
+    				.username("user")
+    				.password("password")
+    				.roles("USER")
+    				.build();
+
+    		return new InMemoryUserDetailsManager(user);
+    	}
 	 
 
 }
