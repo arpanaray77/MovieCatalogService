5cf3ca6122a4cd24a11b9328cebccd58aabe1836
arpanaray77 <arpanaray75897@gmail.com>
2021-04-24 23:16:50 +0530

    Using JPA to read user details from database

--- Movie-catalog-service/src/main/java/com/learnmicroservices/Moviecatalogservice/MyUserDetailsService.java
+++ Movie-catalog-service/src/main/java/com/learnmicroservices/Moviecatalogservice/MyUserDetailsService.java
@@ -1,5 +1,19 @@
 package com.learnmicroservices.Moviecatalogservice;
 
-public class MyUserDetailsService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		return new MyUserDetails(username);
+	}
+	
+	
 
 }
--- Movie-catalog-service/src/main/java/com/learnmicroservices/Moviecatalogservice/SecurityConfig.java
+++ Movie-catalog-service/src/main/java/com/learnmicroservices/Moviecatalogservice/SecurityConfig.java
@@ -1,20 +1,32 @@
 package com.learnmicroservices.Moviecatalogservice;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	   @Autowired
+	   UserDetailsService userDetailsService;
+	   
+	   @Override
+	   public void configure(AuthenticationManagerBuilder auth) throws Exception
+	   {
+		   auth.userDetailsService(userDetailsService);
+	   }
 	
         @Override //overriding default spring security login -----authorization
 		protected void configure(HttpSecurity http) throws Exception {
@@ -28,17 +40,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
         }
         
         @Bean
-    	@Override //getting a user from memory
-    	public UserDetailsService userDetailsService() {
-    		UserDetails user =
-    			 User.withDefaultPasswordEncoder()
-    				.username("user")
-    				.password("password")
-    				.roles("USER")
-    				.build();
-
-    		return new InMemoryUserDetailsManager(user);
-    	}
-	 
-
+        public PasswordEncoder getPasswordEncoder()
+        {
+        	return NoOpPasswordEncoder.getInstance();
+        }
 }
